ff58f97cfb930ed8a31547f9f6d383feb466ed26
RenanMano <renan.fms.oficial@gmail.com>
2026-03-26 19:28:59 -0300

    mais exercicios

--- aula-02-variaveis-operadores/aula02a-variaveis.py
+++ aula-02-variaveis-operadores/aula02a-variaveis.py
@@ -3,6 +3,7 @@
 print(7 + 4)
 print("7 + 4")
 print("7" + "4") # CONCATENAÇÃO DE STRINGS'''
+from fontTools.misc.psCharStrings import t1OperandEncoding
 
 #Comentários de 1 linha
 '''
@@ -61,5 +62,39 @@ def exercicio_06():
     media = (nt01 + nt02) / 2
     print(f"A média aritmética do aluno é {media:.1f}")
 def exercicio_07():
-    print("----Leia 2 valores A e B, que correspondem a 2 noas de um aluno.A seguir calcule e informe a média")
-exercicio_06()
+    print("----Leia 2 valores A e B, que correspondem a 2 noas de um aluno.A seguir calcule e informe a média ponderada do aluno, sabendo que a nota A tem peso 4 e a nota B tem peso 6.\nExemplo: nota a * 4 e nota b * 6")
+    a = float(input("Digite a nota A: "))
+    b = float(input("Digite a nota B: "))
+    media = ((a * 4) + (b * 6)) / 10
+    print(f"A média ponderada é {media:.2f}")
+def exercicio_08():
+    print("Neste problema, deve-se ler o nome de uma peça que chamaremos de peça1, o número de peças1 que o usuário quer, o valor unitário de cada peça1,\no nome de uma peça2,, o número de peças2 e o valor unitário de cada peça2. Após, calcule e mostre o valor a ser pago.")
+    np1 = input("Qual a primeira peça que você quer?")
+    qp1 = int(input(f"quantas {np1} você vai precisar"))
+    vp1 = float(input(f"quanto está custando cada {np1}?"))
+
+    np2 = input("Qual a segunda peça que você quer?")
+    qp2 = int(input(f"quantas {np2} você vai precisar"))
+    vp2 = float(input(f"quanto está custando cada {np2}?"))
+
+    t1 = qp1 * vp1
+    t2 = qp2 * vp2
+
+    print(f"{qp1}un de {np1} vão custar R${t1:.2f} e {qp2}un de {np2} vão custar R${t2:.2f}")
+def exercicio_09():
+    print('''
+    ___________________________________________________________________
+    ----Crie um programa que receba o valor do produto e valor pago----
+    ----Calcule o troco a ser pago                                 ----
+    ----O valor do troco deeve ser exibido no terminal             ----
+    -------------------------------------------------------------------
+    ''')
+    produto = 50.00
+    pagamento = float(input(f"O produto custa R${produto:.2f}, qual valor você usara para pagar?"))
+    troco = pagamento - produto
+
+    if pagamento > produto:
+        print(f"Seu troco é R${troco}")
+    else:
+        print(f"R${pagamento} não é suficiente para comprar o produto.")
+exercicio_09()
