d7101eae990379bfa8646b96958eef35031b75e2
RenanMano <renan.fms.oficial@gmail.com>
2026-04-23 22:36:04 -0300

    aula 04 23/04/2026

---
+++ aula04_repeticao/aula04b_for.py
@@ -0,0 +1,17 @@
+# for cont_music in range(3):
+#     print(f"Música {cont_music}")
+#
+# # de 1 até 11 pulando de 2 em 2
+# for i in range(1, 12, 2):
+#     print(i)
+#
+# # atividade 3
+# qtd_misc = int(input("Digite a quantidade de músicas q vc tem na playlist (DB): "))
+#
+# for i in range(qtd_misc):
+#     print(f"Música {i}")
+
+# repetições encadeadas
+for i in range(0, 4):
+    for j in range(0, 3, 2):
+        print(f"i:{i}, j:{j}")
